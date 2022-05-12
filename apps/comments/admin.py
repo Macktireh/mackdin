deac1988ac537a30d1a07cb4a3c7a2cf217757e6
Macktireh <abdimack97@gmail.com>
2022-05-12 16:38:25 +0200

    renommer le champ full_name en author_comment dans la comments sur le site d'admin

--- apps/comments/admin.py
+++ apps/comments/admin.py
@@ -3,11 +3,11 @@ from apps.comments.models import Comment, ReponseComment, LikeComment
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'post', 'message', 'count_like', 'date_added',)
+    list_display = ('Author_comment', 'post', 'message', 'count_like', 'date_added',)
 
     ordering = ('-date_added',)
     
-    def full_name(self, obj):
+    def Author_comment(self, obj):
         return f"{obj.author.first_name} {obj.author.last_name}"
     
     def count_like(self, obj):
@@ -16,11 +16,11 @@ class CommentAdmin(admin.ModelAdmin):
 admin.site.register(Comment, CommentAdmin)
 
 class ReponseCommentAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'comment', 'message', 'date_added',)
+    list_display = ('Author_comment_reponse', 'comment', 'message', 'date_added',)
 
     ordering = ('-date_added',)
     
-    def full_name(self, obj):
+    def Author_comment_reponse(self, obj):
         return f"{obj.author.first_name} {obj.author.last_name}"
     
 admin.site.register(ReponseComment, ReponseCommentAdmin)
