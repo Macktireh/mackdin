fbedc0698bb63bd8ebefd60cc21712f3d7db26d0
Macktireh <abdimack97@gmail.com>
2023-05-06 16:17:12 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -o errexit  # exit on error
 
-pip3 install -r requirements.txt --no-cache-dir
 pip3 install --upgrade pip
+pip3 install -r requirements.txt --no-cache-dir
 
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
