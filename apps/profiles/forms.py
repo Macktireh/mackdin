5767765f2387826b6bb18f9236452d78d43cd850
Macktireh <abdimack97@gmail.com>
2023-05-06 21:00:24 +0200

    remove the is_fixture field in ProfileForm

--- apps/profiles/forms.py
+++ apps/profiles/forms.py
@@ -6,6 +6,7 @@ from apps.profiles.models import Profile
 
 User = get_user_model()
 
+
 class ProfileForm(forms.ModelForm):
     # img_profile = forms.ImageField(required=False, widget=forms.FileInput(
     #     attrs={'class':'form-control-profile'}))
@@ -15,32 +16,48 @@ class ProfileForm(forms.ModelForm):
     #     attrs={'class':'form-control-profile'}))
     class Meta:
         model = Profile
-        exclude = ('user', 'uid', 'phone', 'adress', 'number_views', 'gender', 'birth_date', 'friends', 'is_updating_img_profile', 'is_updating_img_bg',)
+        exclude = (
+            "user",
+            "uid",
+            "phone",
+            "adress",
+            "number_views",
+            "gender",
+            "birth_date",
+            "friends",
+            "is_updating_img_profile",
+            "is_updating_img_bg",
+            "is_fixture",
+        )
         widgets = {
-            'pseudo': forms.TextInput(attrs={'class': 'form-control-profile'}),
-            'bio': forms.TextInput(attrs={'class': 'form-control-profile'}),
+            "pseudo": forms.TextInput(attrs={"class": "form-control-profile"}),
+            "bio": forms.TextInput(attrs={"class": "form-control-profile"}),
             # 'img_profile': forms.FileInput(attrs={'class': 'form-control-profile'}),
             # 'img_bg': forms.FileInput(attrs={'class': 'form-control-profile'}),
             # 'phone': forms.TextInput(attrs={'class': 'form-control-profile'}),
             # 'adress': forms.TextInput(attrs={'class': 'form-control-profile'}),
-            'town': forms.TextInput(attrs={'class': 'form-control-profile'}),
-            'region': forms.TextInput(attrs={'class': 'form-control-profile'}),
-            'zipcode': forms.TextInput(attrs={'class': 'form-control-profile'}),
-            'country': forms.TextInput(attrs={'class': 'form-control-profile'}),
-            'description': forms.Textarea(attrs={'cols':'80', 'rows':'8', 'class': 'form-control-profile'}),
-            'link_linkedin': forms.TextInput(attrs={'class': 'form-control-profile'}),
-            'link_gitthub': forms.TextInput(attrs={'class': 'form-control-profile'}),
-            'link_twitter': forms.TextInput(attrs={'class': 'form-control-profile'}),
-            'link_mysite': forms.TextInput(attrs={'class': 'form-control-profile'}),
+            "town": forms.TextInput(attrs={"class": "form-control-profile"}),
+            "region": forms.TextInput(attrs={"class": "form-control-profile"}),
+            "zipcode": forms.TextInput(attrs={"class": "form-control-profile"}),
+            "country": forms.TextInput(attrs={"class": "form-control-profile"}),
+            "description": forms.Textarea(
+                attrs={"cols": "80", "rows": "8", "class": "form-control-profile"}
+            ),
+            "link_linkedin": forms.TextInput(attrs={"class": "form-control-profile"}),
+            "link_gitthub": forms.TextInput(attrs={"class": "form-control-profile"}),
+            "link_twitter": forms.TextInput(attrs={"class": "form-control-profile"}),
+            "link_mysite": forms.TextInput(attrs={"class": "form-control-profile"}),
         }
-        
+
+
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name',)
+        fields = (
+            "first_name",
+            "last_name",
+        )
         widgets = {
-            'first_name': forms.TextInput(attrs={'class': 'form-control-profile'}),
-            'last_name': forms.TextInput(attrs={'class': 'form-control-profile'}),
-
+            "first_name": forms.TextInput(attrs={"class": "form-control-profile"}),
+            "last_name": forms.TextInput(attrs={"class": "form-control-profile"}),
         }
-        
